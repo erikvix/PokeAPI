b3c29b7806a28a3bacfc2449d640bbcc63f67d7b
Erik Nunes <erikvixx@gmail.com>
2023-08-31 09:44:37 -0300

    api-search-working

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const url = "https://pokeapi.co/api/v2/pokemon/";
-
 const pokemonhtml = document.querySelector("#pokemon");
+const pokemonphoto = document.querySelector("#pokefoto");
+const pokemonform = document.querySelector("#pokeform");
+var pokeName, pokemon, card;
+const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
+const input = document.querySelector("#input");
+const pesquisabtn = document.querySelector("#pesquisabtn");
 
-function GetPokemon(url, nome) {
-  axios
-    .get(url + nome)
-    .then((response) => {
-      const pokemon = response.data;
-
-      pokemonhtml.innerHTML = "pokemon";
-    })
-    .catch((error) => console.log(error));
+function displayPokemonInfo(data) {
+  console.log(data);
+  pokemonhtml.innerHTML = data.name;
+  pokemonphoto.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+  pokemonform.src = data.sprites.other['official-artwork'].front_default;
+  console.log("ID: " + data.id);
+  console.log("Nome: " + data.name);
+  console.log("Altura: " + data.height);
+  console.log("Peso: " + data.weight);
 }
 
-let nome1 = "pikachu";
+function requestPokeInfo(url, name) {
+  axios
+    .get(url + name)
+    .then((response) => {
+      pokemon = response.data;
+      displayPokemonInfo(pokemon);
+    })
+    .catch((err) => console.log(err));
+}
 
-GetPokemon(url, nome1);
+pesquisabtn.addEventListener("click", function (e) {
+  pokeName = input.value.toLowerCase();
+  requestPokeInfo(baseUrl, pokeName);
+});
