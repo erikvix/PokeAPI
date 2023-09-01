122f0cb8b256bd0deaa517fa5a34b748c28a9e8e
Erik Nunes <erikvixx@gmail.com>
2023-09-01 11:49:28 -0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const pokemonhtml = document.querySelector("#pokemon");
 const pokemonphoto = document.querySelector("#pokefoto");
 const pokemonform = document.querySelector("#pokeform");
+const pokemonweight = document.querySelector("#pokepeso");
+const pokemonheight = document.querySelector("#pokealtura");
 const type = document.querySelector("#poketipo")
 var pokeName, pokemon, card;
 const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
@@ -10,29 +12,20 @@ const pesquisabtn = document.querySelector("#pesquisabtn");
 function displayPokemonInfo(data) {
   console.log(data);
   pokemonhtml.innerHTML = data.name;
-  // poketipo.innerHTML = data.types[0].type.name;
+  pokemonheight.innerHTML = `Altura: ${data.height}m`
+  pokemonweight.innerHTML = `Peso: ${data.weight}kg`
   data.types.forEach((type, index) => {
 
-
-
-
     const poketype = document.createElement("p");
-    
-    // Defina o texto do parágrafo com o nome do tipo
+
     poketype.textContent = `${type.type.name}`;
     poketype.classList.add("element")
     
-
-    // Adicione o parágrafo ao elemento HTML com id "pokemon"
     const pokemonhtml = document.querySelector("#pokemon");
     pokemonhtml.appendChild(poketype);
 });
   pokemonphoto.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
   pokemonform.src = data.sprites.other['official-artwork'].front_default;
-  console.log("ID: " + data.id);
-  console.log("Nome: " + data.name);
-  console.log("Altura: " + data.height);
-  console.log("Peso: " + data.weight);
 }
 
 function requestPokeInfo(url, name) {
