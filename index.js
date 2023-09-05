585396b72e88f7e429d9f73c327ba510398ecd6d
Erik Nunes <erikvixx@gmail.com>
2023-09-05 11:57:48 -0300

    layout-update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const pokemonphoto = document.querySelector("#pokefoto");
 const pokemonform = document.querySelector("#pokeform");
 const pokemonweight = document.querySelector("#pokepeso");
 const pokemonheight = document.querySelector("#pokealtura");
+const pokemonid = document.querySelector("#pokeid")
 const type = document.querySelector("#poketipo")
 var pokeName, pokemon, card;
 const baseUrl = "https://pokeapi.co/api/v2/pokemon/";
@@ -11,6 +12,7 @@ const pesquisabtn = document.querySelector("#pesquisabtn");
 
 function displayPokemonInfo(data) {
   console.log(data);
+  pokemonid.innerHTML = `#${data.id}`
   pokemonhtml.innerHTML = data.name;
   pokemonheight.innerHTML = `Altura: ${data.height}m`
   pokemonweight.innerHTML = `Peso: ${data.weight}kg`
@@ -18,10 +20,10 @@ function displayPokemonInfo(data) {
 
     const poketype = document.createElement("p");
 
-    poketype.textContent = `${type.type.name}`;
+    poketype.textContent = `Tipo: ${type.type.name}`;
     poketype.classList.add("element")
     
-    const pokemonhtml = document.querySelector("#pokemon");
+    const pokemonhtml = document.querySelector(".type-list");
     pokemonhtml.appendChild(poketype);
 });
   pokemonphoto.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
@@ -39,6 +41,9 @@ function requestPokeInfo(url, name) {
 }
 
 pesquisabtn.addEventListener("click", function (e) {
+  const box = document.querySelector(".pokemon-box");
+
+  box.classList.remove("display-off")
   pokeName = input.value.toLowerCase();
   requestPokeInfo(baseUrl, pokeName);
 });
