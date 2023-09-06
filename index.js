bdc9ffe4dfec16648d2e4ee1ad7027b3e8577030
Erik Nunes <erikvixx@gmail.com>
2023-09-06 11:58:02 -0300

    type

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ const pesquisabtn = document.querySelector("#pesquisabtn");
 
 function displayPokemonInfo(data) {
   console.log(data);
-  pokemonid.innerHTML = `#${data.id}`
+  pokemonid.textContent = (data.id < 10 ? "#00" : data.id < 100 ? "#0" : "#").concat(data.id)
   pokemonhtml.innerHTML = data.name;
-  pokemonheight.innerHTML = `Altura: ${data.height}m`
-  pokemonweight.innerHTML = `Peso: ${data.weight}kg`
+  pokemonheight.innerHTML = `Altura: ${data.height / 10}m`
+  pokemonweight.innerHTML = `Peso: ${(data.weight / 10) }kg`
   data.types.forEach((type, index) => {
 
     const poketype = document.createElement("p");
 
-    poketype.textContent = `Tipo: ${type.type.name}`;
-    poketype.classList.add("element")
+    poketype.textContent = `${type.type.name}`;
+
+    poketype.classList.add(type.type.name === "psychic" ? "tag" : "psychic", "tag");
     
     const pokemonhtml = document.querySelector(".type-list");
     pokemonhtml.appendChild(poketype);
