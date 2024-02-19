0d02a29d37e127dd79b8f14bd0c8e5461560bd64
Erik Nunes <erikvixx@gmail.com>
2024-02-19 17:21:42 -0300

    init files structure

--- src/componentsHTML/List/index.tsx
+++ src/componentsHTML/List/index.tsx
@@ -28,65 +28,97 @@ type listPokemon = {
       };
     };
   };
+  types: {
+    type: {
+      name: string;
+    };
+  };
 };
 
 const index = () => {
   const [data, setData] = useState<listPokemon[]>([]);
-  const store = usePokeStore()
+  const store = usePokeStore();
   const [loading, setLoading] = useState(false);
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const response = await fetch(
+  //         `https://pokeapi.co/api/v2/pokemon/` + store.name
+  //       );
+  //       const json = await response.json();
+  //       if (response.status !== 200) throw new Error(json.message);
+  //       setData([json]);
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.log("Aconteceu o seguinte erro: " + error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   return fetchData;
+  // }, [store.name]);
+
   useEffect(() => {
-    
-    const fetchData = async () => {
-      try {
-        setLoading(true)
-        setData([])
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/` + store.name);
-        const json = await response.json();
-        if (response.status !== 200) throw new Error(json.message);
-        setData([json]);
+    fetch(`https://pokeapi.co/api/v2/pokemon/` + store.name)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data: listPokemon) => {
+        setData([data]);
         console.log(data);
-      } catch (error) {
-        console.log('Aconteceu o seguinte erro: ' + error);
-        setData([])
-      } finally{
-        setLoading(false)
-      }
-    }
-    
-    return fetchData
-   
-  }, [store.name]);
+      });
+  }, []);
   return (
     <section className="mt-10">
-      <div className="grid grid-cols-2 xl:grid-cols-4 gap-10">
-    
-        {loading && <Skeleton/> }
-        {data ? (data.map((item, index) => (
-          <Card key={item.id | index}>
-            <CardHeader>
-              <CardTitle className="text-center text-4xl capitalize font-semibold tracking-tight ">
-                {item.name}
-              </CardTitle>
-              {item.sprites && item.sprites.versions["generation-v"]["black-white"].animated["front_default"] ? (
-                <div>
-                  <img
-                    src={`${item.sprites.versions["generation-v"]["black-white"].animated["front_default"]}`}
-                    alt=""
-                  />
-                  <img src={`${item.sprites["front_default"]}`} alt={item.name} />
-                </div>
-              ) : null}
-            </CardHeader>
-            <CardContent>
-              <CardDescription>{item.name}</CardDescription>
-            </CardContent>
-            <CardFooter>
-              <p>Card Footer</p>
-            </CardFooter>
-          </Card>
-        ))): null} 
-       
+      <div className="justify-center flex flex-col items-center">
+        {loading && <Skeleton />}
+        {data
+          ? data.map((item, index) => (
+              <Card className="max-w-max flex" key={item.id | index}>
+                <CardHeader>
+                  <CardTitle className="text-center text-4xl capitalize font-semibold tracking-tight ">
+                    {`#${item.id}`}
+                  </CardTitle>
+                  <CardTitle className="text-center text-4xl capitalize font-semibold tracking-tight ">
+                    {item.name}
+                  </CardTitle>
+                  <CardTitle className="text-center text-4xl capitalize font-semibold tracking-tight ">
+                    {item.types.type.name}
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  {item.sprites &&
+                  item.sprites.versions["generation-v"]["black-white"].animated[
+                    "front_default"
+                  ] ? (
+                    <div className="flex items-center flex-col justify-center">
+                      <img
+                        className="w-full h-auto"
+                        src={`${item.sprites["front_default"]}`}
+                        alt={item.name}
+                      />
+                      <img
+                        src={`${item.sprites.versions["generation-v"]["black-white"].animated["front_default"]}`}
+                        className="w-full h-auto"
+                        alt=""
+                      />
+                    </div>
+                  ) : (
+                    <div className="flex gap-4  ">
+                      <Skeleton className="h-[250px] w-[250px] rounded-xl" />
+                      <Skeleton className="h-[250px] w-[250px] rounded-xl" />
+                    </div>
+                  )}
+                </CardContent>
+                <CardFooter>
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
+            ))
+          : null}
       </div>
     </section>
   );
